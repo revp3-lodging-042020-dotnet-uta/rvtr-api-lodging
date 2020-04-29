4a6b85e2aa7044c99a264f12f8207bbcd12981a6
fred belotte <fredbelotte@outlook.com>
2020-04-29 00:49:28 -0500

    add latest changes to changelog unreleased section [finishes #172469617]

--- aspnet/RVTR.Lodging.UnitTesting/Tests/RepositoryTest.cs
+++ aspnet/RVTR.Lodging.UnitTesting/Tests/RepositoryTest.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
--- aspnet/RVTR.Lodging.UnitTesting/Tests/UnitOfWorkTest.cs
+++ aspnet/RVTR.Lodging.UnitTesting/Tests/UnitOfWorkTest.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using RVTR.Lodging.DataContext;
