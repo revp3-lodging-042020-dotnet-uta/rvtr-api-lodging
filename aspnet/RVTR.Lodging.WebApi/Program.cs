4b246d78a543ab5921e63fce4ab55c06cc759920
fred belotte <fredbelotte@outlook.com>
2020-05-02 16:34:10 -0500

    configure database context creation [#172282524]

--- aspnet/RVTR.Lodging.WebApi/Program.cs
+++ aspnet/RVTR.Lodging.WebApi/Program.cs
@@ -1,20 +1,36 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RVTR.Lodging.DataContext;
 
 namespace RVTR.Lodging.WebApi
 {
   public class Program
   {
-    public static void Main(string[] args)
+    public static async void Main()
     {
-      CreateHostBuilder(args).Build().Run();
+      var host = CreateHostBuilder().Build();
+
+      await CreateDbContextAsync(host);
+      await host.RunAsync();
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-      Host.CreateDefaultBuilder(args)
-          .ConfigureWebHostDefaults(webBuilder =>
-          {
-            webBuilder.UseStartup<Startup>();
-          });
+    public static IHostBuilder CreateHostBuilder() =>
+      Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webBuilder =>
+      {
+        webBuilder.UseStartup<Startup>();
+      });
+
+    public static async Task CreateDbContextAsync(IHost host)
+    {
+      using (var scope = host.Services.CreateScope())
+      {
+        var provider = scope.ServiceProvider;
+        var context = provider.GetRequiredService<LodgingContext>();
+
+        await context.Database.EnsureCreatedAsync();
+      }
+    }
   }
 }
