0c2d75cd8d31930a9d016e40e65acbd0e3f0bcec
Jayson Lennon <jayson@jaysonlennon.dev>
2020-06-15 18:21:46 -0700

    Move logic into getters

--- aspnet/RVTR.Lodging.DataContext/SearchFilterModel.cs
+++ aspnet/RVTR.Lodging.DataContext/SearchFilterModel.cs
@@ -5,32 +5,70 @@ namespace RVTR.Lodging.DataContext
     /// </summary>
     public class SearchFilterModel
     {
-        private int? _limit;
+        private int _limit;
 
         /// <summary>
         /// Max number of results to be returned.
         /// </summary>
-        public int? Limit
+        public int Limit
         {
-            get { return _limit; }
-            set {
-              if (value == null) return;
-              _limit = (int)value < 0 ? 1 : value;
-            }
+            get { return _limit <= 0 ? 50 : _limit; }
+            set { _limit = value; }
         }
 
-        private int? _paginate;
+        private int _paginate;
 
         /// <summary>
         /// Return results starting from this value.
         /// </summary>
-        public int? Paginate
+        public int Paginate
         {
-            get { return _paginate; }
-            set {
-              if (value == null) return;
-              _paginate = (int)value < 0 ? 0 : value;
+            get { return _paginate < 0 ? 0 : _paginate; }
+            set { _paginate = value; }
+        }
+
+        private int _ratingAtLeast;
+        /// <summary>
+        /// Minimum star rating.
+        /// </summary>
+        public int RatingAtLeast
+        {
+            get {
+                if (_ratingAtLeast < 1) return 1;
+                if (_ratingAtLeast > 5) return 5;
+                return _ratingAtLeast;
             }
+            set { _ratingAtLeast = value; }
+        }
+
+        private int _searchRadius;
+        /// <summary>
+        /// Search radius (in miles).
+        /// </summary>
+        public int SearchRadius
+        {
+            get { return _searchRadius < 0 ? 1 : _searchRadius; }
+            set { _searchRadius = value; }
+        }
+        
+        private int _bedsAtLeast;
+        /// <summary>
+        /// Minimum number of beds in a lodging.
+        /// </summary>
+        public int BedsAtLeast
+        {
+            get { return _bedsAtLeast < 0 ? 0 : _bedsAtLeast; }
+            set { _bedsAtLeast = value; }
+        }
+
+        private int _bathsAtLeast;
+        /// <summary>
+        /// Minimum number of baths in a lodging.
+        /// </summary>
+        public int BathsAtLeast
+        {
+            get { return _bathsAtLeast < 0 ? 0 : _bathsAtLeast; }
+            set { _bathsAtLeast = value; }
         }
 
         /// <summary>
@@ -44,11 +82,8 @@ namespace RVTR.Lodging.DataContext
         /// </summary>
         public string SortOrder
         {
-            get { return _sortOrder; }
-            set
-            {
-                _sortOrder = value == "desc" ? "desc" : "asc";
-            }
+            get { return _sortOrder == "desc" ? "desc" : "asc"; }
+            set { _sortOrder = value; }
         }
     }
 }
