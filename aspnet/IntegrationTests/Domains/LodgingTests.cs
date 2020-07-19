f740eeb14f02a55037037a3617a99fc572afed16
Josh Bertrand <joshuabertrand@protonmail.com>
2020-07-18 20:46:56 -0400

    Configure dependencies and set up basic LodgingTest skeleton

--- aspnet/IntegrationTests/Domains/LodgingTests.cs
+++ aspnet/IntegrationTests/Domains/LodgingTests.cs
@@ -1,10 +1,65 @@
-﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RVTR.Lodging.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace IntegrationTests.Domains
 {
-  class LodgingTests
+  [TestClass]
+  public class LodgingTests : IClassFixture<CustomWebApplicationFactoryInMemDB<Startup>>
   {
+    private readonly CustomWebApplicationFactoryInMemDB<Startup> _factory;
+
+    public LodgingTests(CustomWebApplicationFactoryInMemDB<Startup> factory)
+    {
+      _factory = factory;
+    }
+
+    [Theory]
+    [InlineData()]
+    public Task Post_LodgingControllerSuccessfullyCreatesDbEntry(string url)
+    {
+      //Arrange
+
+      //Act
+
+      //Assert
+
+    }
+
+    [Fact]
+    public Task Get_SuccessfullyRetrievesAllLodgingFromDbEntries()
+    {
+      //Arrange
+
+      //Act
+
+      //Assert
+    }
+
+    [Theory]
+    [InlineData()]
+    public Task Get_RetrievesLodgingFromId(int id)
+    {
+      //Arrange
+
+      //Act
+
+      //Assert
+    }
+
+    [Theory]
+    [InlineData()]
+    public Task Delete_SuccessfullyRemovesLodgingFromDb(int id)
+    {
+      //Arrange
+
+      //Act
+
+      //Assert
+    }
   }
 }
